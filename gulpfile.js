404d14fe2dc24bf2536fddd7e3fbc854c6d6224e
Warren Shea <warren.shea@gmail.com>
2019-12-21 20:25:10 -0500

    Major updates for Gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ const gulp = require('gulp'),
       //JavaScript
       babel = require('gulp-babel'),
       uglify = require('gulp-uglify'),
-      
+
       //Helpers
       harp = require('harp'),
       exec = require('child_process').exec,
@@ -19,59 +19,91 @@ const gulp = require('gulp'),
       argv = require('yargs').argv,
       del = require('del');
 
-gulp.task('css', () => {
-  if (process.env.NODE_ENV === 'development') {
-    return gulp.src('./_dev/resources/stylesheets/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
-    .pipe(gulp.dest('./_dev/resources/stylesheets'))
-    .pipe(reload({stream: true}))
+const browserSyncReload = (done) => {
+  reload();
+}
+
+const clean = (done) => {
+  if (process.env.NODE_ENV === 'production') {
+    return del([
+      '_dist/*',
+      '_dist/**/*'
+    ]);
   } else {
-    return gulp.src('./_dev/resources/stylesheets/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
-    .pipe(cleanCSS())
-    .pipe(gulp.dest('./_dist/resources/stylesheets'))
+    done();
   }
-});
+}
 
-gulp.task('js', () => {
+const css = (done) => {
   if (process.env.NODE_ENV === 'development') {
-    return gulp.src('./_dev/resources/scripts/**/*.js')
-      .pipe(babel({
-        compact: false,
-        presets: ['es2015']
-      }))
-      .pipe(reload({stream: true}))
+    return gulp.src(['./_dev/resources/stylesheets/**/*.scss'])
+      .pipe(sass().on('error', sass.logError))
+      .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+      /*.pipe(cleanCSS({
+        //Remove comments and minify
+        level: {
+          1: {specialComments: 0}
+        }}))*/
+      .pipe(gulp.dest('./_dev/resources/stylesheets'))
+      .pipe(reload({stream: true}));
   } else {
-    return gulp.src('./_dev/resources/scripts/**/*.js')
-      .pipe(babel({
-        compact: false,
-        presets: ['es2015']
-      }))
-      .pipe(uglify())
-      .pipe(gulp.dest('./_dist/resources/scripts'))
-  }  
-});
-
-gulp.task('html', function() {
-  if (process.env.NODE_ENV === 'development') {
-    return browserSync.reload();
+    return gulp.src(['./_dev/resources/stylesheets/**/*.scss'])
+      .pipe(sass().on('error', sass.logError))
+      .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+      .pipe(cleanCSS({
+        //Remove comments and minify
+        level: {
+          1: {specialComments: 0}
+        }}))
+      .pipe(gulp.dest('./_dist/resources/stylesheets'));
   }
-});
+}
 
-gulp.task('watch', function() {
-  gulp.watch(['./_dev/resources/stylesheets/*.scss', './_dev/resources/stylesheets/**/*.scss'], ['css']);
-  gulp.watch('./_dev/resources/scripts/*.js', ['js']);
-  gulp.watch('./_dev/**/*.ejs', ['html']);
-});
+const js = (done) => {
+  if (process.env.NODE_ENV === 'development') {
+    return gulp.src(['./_dev/resources/scripts/**/*.js'])
+      .pipe(reload({stream: true}));
+  } else {
+    return gulp.src(['./_dev/resources/scripts/**/*.js'])
+      .pipe(babel({"presets": ['es2015']}))
+      //.pipe(uglify()) //Minify
+      .pipe(gulp.dest('./_dist/resources/scripts'));
+    return;
+  }
+}
 
-gulp.task('dev-set-env', function () {
-  return process.env.NODE_ENV = 'development';
-});
+const images = () => {
+  return gulp.src('./_dev/assets/images/**/*')
+    .pipe(gulp.dest('./_dist/assets/images/'))
+    .on('error', function(e){console.log("ERROR 2",e);});
+}
 
-gulp.task('serve', function (done) {
+const html = (done) => {
+  if (process.env.NODE_ENV === 'development') {
+    browserSyncReload();
+  }
+  done();
+}
+
+const watchFiles = () => {
+  gulp.watch(['./_dev/resources/stylesheets/*.scss', './_dev/resources/stylesheets/**/*.scss','./_dev/style-guide/**/*.css'], css);
+  gulp.watch(['./_dev/resources/scripts/*.js', './_dev/resources/scripts/**/*.js',,'./_dev/style-guide/**/*.js'], js);
+  gulp.watch('./_dev/**/*.ejs', html);
+}
+
+const devSetEnv = (done) => {
+  process.env.NODE_ENV = 'development';
   console.log("Environment: ",process.env.NODE_ENV);
+  done();
+}
+
+const prodSetEnv = (done) => {
+  process.env.NODE_ENV = 'production';
+  console.log("Environment: ",process.env.NODE_ENV);
+  done();
+}
+
+const serve = (done) => {
   harp.server(__dirname + "/_dev", {
     port: 9100
   }, function (done){
@@ -81,18 +113,25 @@ gulp.task('serve', function (done) {
       startPath: '/'
     });
   });
-});
+  done();
+}
 
-gulp.task('prod-set-env', function () {
-  return process.env.NODE_ENV = 'production';
-});
-
-gulp.task('build', ['prod-set-env'], () => {
-  console.log("Environment: ",process.env.NODE_ENV);
+const build = (done) => {
   exec('npm run compile', function(err, stdout, stderr) {
-    console.log(stdout);
-    console.log(stderr);
+    //console.log(stdout);
+    //console.log(stderr);
+    css();
+    js();
+    console.log("Build completed!");
   });
-});
+  done();
+}
 
-gulp.task('default', (argv.prod === true) ? ['prod-set-env','build','css','js'] : ['dev-set-env','serve','css','js','html','watch']);
+/* This won't work, build will override css and js. Moved CSS and JS to Callback of Build
+This is unique to HarpJS and how the folder code is structured
+const defaultProd = gulp.series(prodSetEnv, clean, build, css, js);*/
+const defaultProd = gulp.series(prodSetEnv, clean, build);
+
+const defaultDev = gulp.series(devSetEnv, gulp.parallel(serve, css, js, html, watchFiles));
+
+exports.default = (argv.prod === true) ? defaultProd : defaultDev;
